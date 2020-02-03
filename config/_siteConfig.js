4e876c2a26e6fb31dbef53dc077b62312e3e16fd
Will Gelder <willgelder91@gmail.com>
2020-02-03 10:12:54 -0500

    adding link preview image

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -13,7 +13,7 @@ export default {
   // prettier-ignore
   tagline: 'Senior UX Designer with 5+ years of experience',
   // featureImage: '/uploads/home-hero.jpg',
-  featureImage: '',
+  featureImage: '/uploads/preview-image.png',
   logo: 'logo.svg', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
